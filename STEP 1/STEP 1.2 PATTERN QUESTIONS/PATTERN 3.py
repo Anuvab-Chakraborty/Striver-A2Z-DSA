fbeada27055bc4dfa5eef5be8d31f947a60f8456
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-02-06 19:06:53 +0530

    pattern 3 uploaded

---
+++ STEP 1/STEP 1.2 PATTERN QUESTIONS/PATTERN 3.py
@@ -0,0 +1,16 @@
+Question_link = "https://practice.geeksforgeeks.org/problems/triangle-number/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_3"
+# PATTERN:
+"""
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+
+"""
+# Solution:
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print("")
