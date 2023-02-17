dc834841490e1607e7e42fd615c32b25a174d9e8
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-02-17 18:58:18 +0530

    1-n printing added

---
+++ STEP 1/STEP 1.5 BASIC RECURSION/PRINT 1-N.py
@@ -0,0 +1,40 @@
+QUESTION_LINK = "https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-1-to-n-without-using-loops"
+
+"""
+Print numbers from 1 to N without the help of loops.
+
+Example 1:
+
+Input:
+N = 10
+Output: 1 2 3 4 5 6 7 8 9 10
+
+Example 2:
+
+Input:
+N = 5
+Output: 1 2 3 4 5
+ 
+
+Your Task:
+This is a function problem. You only need to complete the function printNos() that takes N as parameter and prints number from 1 to N recursively. Don't print newline, it will be added by the driver code.
+
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N) (Recursive).
+
+
+Constraints:
+1 <= N <= 1000
+"""
+
+
+class Solution:
+    # Complete this function
+    def printNos(self, n):
+        # Your code here
+        if n == 0:
+            return n
+        else:
+            self.printNos(n - 1)
+            print(n, end=" ")
