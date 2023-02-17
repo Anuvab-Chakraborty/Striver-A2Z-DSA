f02cc57c7b1095c55ae4312fa4d5d369e1579c2e
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-02-17 19:39:19 +0530

    SUM OF FIRST N NUMBERS added

---
+++ STEP 1/STEP 1.5 BASIC RECURSION/SUM OF FIRST N NUMBERS.py
@@ -0,0 +1,51 @@
+QUESTION_LINK = "https://practice.geeksforgeeks.org/problems/sum-of-first-n-terms5843/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=sum-of-first-n-terms"
+
+"""
+
+Given and integer N.Calculate the sum of series 13 + 23 + 33 + 43 + … till N-th term.
+
+Example 1:
+
+Input:
+N=5
+Output:
+225
+Explanation:
+13+23+33+43+53=225
+Example 2:
+
+Input:
+N=7
+Output:
+784
+Explanation:
+13+23+33+43+53+63+73=784
+
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function sumOfSeries() which takes the integer N as parameter and returns the sum of the cubes of the first N natural numbers.
+
+
+Expected Time Complexity:O(1)
+Expected Auxillary Space:O(1)
+
+
+Constraints:
+1<=N<=50000
+
+"""
+
+
+class Solution:
+    def sumOfSeries(self, n):
+        # code here
+        sum = 0
+        for i in range(1, n + 1):
+            sum += i * i * i
+        return sum
+
+
+"""
+        if n==0:return 0 
+        else:
+            return n**3 + self.sumOfSeries(n-1)
+        """
