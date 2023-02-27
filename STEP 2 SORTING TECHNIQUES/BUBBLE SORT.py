f069b54a6e0230199b44fa3a0e82d85b5037bf1d
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-02-27 21:19:32 +0530

    INSERTION SORT.py  added

--- STEP 2 SORTING TECHNIQUES/BUBBLE SORT.py
+++ STEP 2 SORTING TECHNIQUES/BUBBLE SORT.py
@@ -47,3 +47,7 @@ for i in range(n - 1):
     for j in range(n - i - 1):
         if arr[j] > arr[j + 1]: arr[j], arr[j + 1] = arr[j + 1], arr[j]
 print(arr)
+
+# TIME COMPLEXITY : O(N**2)
+
+# SPACE COMPLEXITY : O(1)
---
+++ STEP 2 SORTING TECHNIQUES/INSERTION SORT.py
@@ -0,0 +1,46 @@
+QUESTION_LINK = "https://practice.geeksforgeeks.org/problems/insertion-sort/0?category[]=Algorithms&page=1&query=category[]Algorithmspage1&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=insertion-sort"
+"""
+
+The task is to complete the insert() function which is used to implement Insertion Sort.
+
+
+Example 1:
+
+Input:
+N = 5
+arr[] = { 4, 1, 3, 9, 7}
+Output:
+1 3 4 7 9
+Example 2:
+
+Input:
+N = 10
+arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+Output:
+1 2 3 4 5 6 7 8 9 10
+
+Your Task: 
+You don't have to read input or print anything. Your task is to complete the function insert() and insertionSort() where insert() takes the array, it's size and an index i and insertionSort() uses insert function to sort the array in ascending order using insertion sort algorithm. 
+
+Expected Time Complexity: O(N*N).
+Expected Auxiliary Space: O(1).
+
+
+Constraints:
+1 <= N <= 1000
+1 <= arr[i] <= 1000
+
+"""
+
+# Solution : -
+
+
+arr = [9, 1, 4, 7, 2, 3, 8, 5]
+for i in range(1, len(arr)):
+    temp = arr[i]
+    j = i - 1
+    while j >= 0 and temp < arr[j]:
+        arr[j + 1] = arr[j]
+        j -= 1
+    arr[j + 1] = temp
+print(arr)
