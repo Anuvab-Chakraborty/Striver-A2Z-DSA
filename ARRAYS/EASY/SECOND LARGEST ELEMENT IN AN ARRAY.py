d360aff9b2406b20bcd109582893cc7161ee08b4
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-05-20 20:48:59 +0530

    SECOND LARGEST ELEMENT IN AN ARRAY.py updated this file

--- ARRAYS/EASY/SECOND LARGEST ELEMENT IN AN ARRAY.py
+++ ARRAYS/EASY/SECOND LARGEST ELEMENT IN AN ARRAY.py
@@ -1,6 +1,15 @@
-question=" https://practice.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest"
+question = " https://practice.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest"
 
-
+HINT = """
+In case to find the second largest element we have to traverse through the array
+and maintain a maximum and second maximum if the current number is greater than
+current maximum put the maximum in the second maximum and update the maximum now
+if the current number is less than maximum in that case we will check if it's greater
+the second maximum and also remember to check if it's not the maximum itself then update
+the number in the second maximum then in case of return remember to return second 
+maximum only if it's not equal to the maximum. Otherwise return -1
+"""
+Solution = """
 class Solution:
 
 	def print2largest(self,arr, n):
@@ -12,3 +21,4 @@ class Solution:
 	        if i!=maxi and i>second_m:
 	            second_m=i
 	    return second_m if second_m!=maxi else -1
+"""
