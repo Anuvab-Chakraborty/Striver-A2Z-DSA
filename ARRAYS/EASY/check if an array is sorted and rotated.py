ed0f05a692615cb3d9f2cdfa8ab643d944e1426a
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-05-20 20:42:56 +0530

    check if an array is sorted and rotated.py added

--- ARRAYS/EASY/check if an array is sorted and rotated.py
+++ ARRAYS/EASY/check if an array is sorted and rotated.py
@@ -3,7 +3,7 @@ Find the breaking point .
 If the array is sorted and then rotated then 
 there shouldn't be more than one breaking point
 so using that as our reference we can solve this 
-question in O(n) TC. We must search in an array there 
+question in O(n) TC.We must search in an array there 
 mustn't be more than one number greater less than 
 it's previous number. the one will be the point where 
 the array was rotated also notice there can be 0 break points 
